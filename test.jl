59005e0d6684cc0fbb914b4f2b64097d7ca17c62
Olli Wilkman <olli.wilkman@iki.fi>
2013-02-25 14:35:54 +0200

    Update test case

--- test.jl
+++ test.jl
@@ -7,15 +7,15 @@ rad(x) = pi*x/180
 
 function main()
     data = readcsv("44_Nysa.csv")
+    data[:,1] = data[:,1] * pi/180
     basis = default_basis()
-    X = linspace(0.0, 60.0, 100)
-    T = rad(X)
+    X = linspace(0.0, max(data[:,1]), 100)
     tic()
-    A = fit_HG1G2(data, basis, true)
-    G = a1a2a3_to_HG1G2(A)
+    G = fit_HG1G2(basis, data)
+    A = HG1G2_to_a1a2a3(G)
     toc()
     println(A)
     println(G)
-    Y = fitted_curve(T, G, basis)
+    Y = fitted_curve(X, G, basis)
     plot(data[:,1],data[:,2],"x",X,Y)
 end
